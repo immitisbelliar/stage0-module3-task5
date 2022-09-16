7675f58c2039891d85c6bd73b485dc45c7faee5f
Yaroslav <mr.yaroslav.g.a@gmail.com>
2022-09-16 19:13:40 +0600

    Rework task5 module3

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        toBeDivided /= 5;
-        System.out.println(toBeDivided);
+        System.out.println(toBeDivided / 5);
     }
 }
